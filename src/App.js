fa8effb00577d5d9bf7d1fe235299b4e49f48aba
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2024-08-23 19:29:10 +0300

    add style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./components/counter/counter";
+// import Counter from "./components/counter/counter";
 import Login_Signup from "./components/login&signup/login&signup/login&signup";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      {/* <Login_Signup /> */}
+      {/* <Counter /> */}
+      <Login_Signup />
     </div>
   );
 }
--- src/components/login&signup/signup/signup.js
+++ src/components/login&signup/signup/signup.js
@@ -1,5 +1,5 @@
-import "../../style.css";
 import { Component } from "react";
+import "../../style.css";
 
 class Signup extends Component {
   render() {
