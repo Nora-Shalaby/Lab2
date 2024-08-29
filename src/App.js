492ed4a15c208dafa0af71fb52abd02bcfa7e31f
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2024-08-29 14:55:56 +0300

    Add Get & Post Examples

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import First from "./components/context/first/first";
 // import Counter from "./components/counter/counter";
 // import Login_Signup from "./components/login&signup/login&signup/login&signup";
 // import Cards from "./components/cards/cards";
+import GetExample from "./components/GETExample/GETExample";
+import PostExample from "./components/POSTExample/POSTExample";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       {/* <Counter /> */}
       {/* <Login_Signup /> */}
       {/* <Cards /> */}
-      <First />
+      {/* <First /> */}
+      <GetExample />
+      <PostExample />
     </div>
   );
 }
